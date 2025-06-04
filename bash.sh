cf05e4bfc9e4d86561ce0ee29f77d6b95542c591
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-04 21:25:52 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -8,3 +8,13 @@ ZIP_NAME="kubu-hai-template-$(date +%s).zip"
 zip -r $ZIP_NAME . -x '*.venv*' '*.pyc' '__pycache__/*' '.git/*'
 
 echo "Packaged as $ZIP_NAME"
+
+git clone https://github.com/Web4application/kubu-hai-template.git
+cd kubu-hai-template
+
+# Copy or move your existing files into this directory
+cp -r /path/to/your/project/* .
+
+git add .
+git commit -m "Initial upload of kubu-hai-template"
+git push origin main
