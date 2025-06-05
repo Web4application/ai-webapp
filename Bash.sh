d997ef36811a8846615571eaa1740245b9776aa6
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-05 06:26:02 +0200

    Add files via upload

---
+++ Bash.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/QUBUHUB-repos/RODA-AI-Web.git
+npm install dotenv
+npm install @liveblocks/client @liveblocks/react @liveblocks/yjs yjs @monaco-editor/react y-monaco y-protocols
+pip install python-dotenv psycopg2
+npx @sentry/wizard@latest -i nextjs --saas --org web4app --project swiftbot
