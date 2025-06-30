3333cfa6f9a921a311b81eb4cc955809fc063d83
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-30 16:44:52 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,16 @@
+npm install -g @angular/cli
+
+Read more here: https://locall.host/index-php/
+
+ng new my-app
+
+Read more here: https://locall.host/index-php/
+
+cd my-app ng serve
+
+Read more here: https://locall.host/index-php/
+
+
 make install-system     # System-wide install (/usr/local/bin)
 make uninstall-system   # Remove system install
 #!/bin/bash
